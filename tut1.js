a07bcec2a8348506c428669c27961d174021283a
Meshv Patel <77038149+meshv-p@users.noreply.github.com>
2021-08-30 12:28:05 +0530

    Update tut1.js

--- tut1.js
+++ tut1.js
@@ -1,39 +1,60 @@
-// console.log("hey");
+
+    
+  function alertbar(text){
+    var txt = text;
+    document.getElementById('alert').innerText = txt;
+    document.querySelector(".alert").style.display = "block";
+  setTimeout(() => {
+      document.querySelector(".alert").style.display = "none";
+  }, 3000);
+  }
+
 
 function upper(){
+    
     var txt = document.getElementById('txt').value;
     var upper = txt.toUpperCase();
-    console.log(upper);
+    // console.log(upper);
     document.getElementById('txt').value = upper;
+    var me = "Your text has been converted into Upper case successfully";
+    alertbar("Your text has been converted into Upper case successfully");
+
 }
 function lower(){
     var txt = document.getElementById('txt').value;
     var lower = txt.toLowerCase();
     console.log(lower);
     document.getElementById('txt').value = lower;
+    alertbar('Your text has been converted into Lower case successfully');
+
 }
 function Replace(){
     var txt = document.getElementById('txt').value;
     var n = prompt('which word you want to be replaced?');
     console.log(n);
     var r = prompt('what you want to change?')
-   
+    alertbar(`Your ${n} has been replcaed ${r} successfully`);
+ 
     console.log(txt.replace(n, r));
     var n = txt.replace(n, r);
-    // r.style.backGround = 'yellow';
+   
     document.getElementById('txt').value =n;
+
 }
 function length(){
 var txt = document.getElementById('txt').value;
 var l = txt.length;
-console.log(l);
+// console.log(l);
 document.getElementById('pre').innerHTML=l;
 var i=0;
 for(i=0 ;i <=txt.length ; i++){
+    if (txt.split(" ") <1){
+        // console.log()
+    }
    var s = txt.split(" ");
     
 }
-console.log(s.length);
+// console.log(s.length);
 document.getElementById('word').innerHTML = s.length;
 }
 
@@ -43,4 +64,6 @@ txt.select();
 txt.setSelectionRange(0, 99999);
 document.execCommand('copy');
 console.log(txt.value);
+alertbar('Your text has been copied.');
+
 }
