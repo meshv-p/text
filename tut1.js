974b691ac3d0dae98fef0f2356126fc5129226f3
Meshv Patel <77038149+meshv-p@users.noreply.github.com>
2021-08-30 11:08:38 +0530

    Add files via upload

---
+++ tut1.js
@@ -0,0 +1,39 @@
+// console.log("hey");
+
+function upper(){
+    var txt = document.getElementById('txt').value;
+    var upper = txt.toUpperCase();
+    console.log(upper);
+    document.getElementById('txt').value = upper;
+}
+function lower(){
+    var txt = document.getElementById('txt').value;
+    var lower = txt.toLowerCase();
+    console.log(lower);
+    document.getElementById('txt').value = lower;
+}
+function Replace(){
+    var txt = document.getElementById('txt').value;
+    var n = prompt('which word you want to be replaced?');
+    console.log(n);
+    var r = prompt('what you want to change?')
+    blink(r);
+    console.log(txt.replace(n, r));
+    var n = txt.replace(n, r);
+    // r.style.backGround = 'yellow';
+    document.getElementById('txt').value =n;
+}
+function length(){
+var txt = document.getElementById('txt').value;
+var l = txt.length;
+console.log(l);
+document.getElementById('pre').innerHTML=l;
+}
+
+function Copy(){
+var txt = document.getElementById('txt');
+txt.select();
+txt.setSelectionRange(0, 99999);
+document.execCommand('copy');
+console.log(txt.value);
+}
